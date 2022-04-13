b48d875ae4284b717f757ac37968926ceff452eb
Jair-77 <100737291+Jair-77@users.noreply.github.com>
2022-04-12 20:32:56 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,35 +1,34 @@
 //console.log("Hola desde Service Worker UTVAM");
 
-//self.addEventListener('install', event => {
+self.addEventListener('install', event => {
     //console.log("Hola desde la intalación del Service Worker UTVAM");
     
     //caches.open( "Espacio 1" );
     //caches.delete("Espacio 2");
-    //self.skipWaiting();
-  //});
+    self.skipWaiting();
+  });
  
- //self.addEventListener('activate', event => {
- //  console.log("Hola desde la Activación del Service Worker UTVAM");
- //});
+ self.addEventListener('activate', event => {
+   console.log("Hola desde la Activación del Service Worker UTVAM");
+ });
  
  
  self.addEventListener('fetch' , event => {
    event.respondWith( fetch(event.request) );  
  
-   //if( event.request.url.includes('http://localhost:8080/images/')){
+   if( event.request.url.includes('http://localhost:8080/images/')){
     // console.log(event.request.url);
-  // }  
+   }  
  });
  
- //self.addEventListener('sync', event => {
+ self.addEventListener('sync', event => {
    
-   //console.log("Recuperando conexion....");
+   console.log("Recuperando conexion....");
  
- //});
+ });
  
- //self.addEventListener('push', event => {
+ self.addEventListener('push', event => {
    
-   //console.log("Notificado....");
+   console.log("Notificado....");
  
- //});
- 
+ });
